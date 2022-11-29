980381d05b22fb9968eb97fd8ca5f451b58f224c
excitedpup <daniellewhitmarsh3@gmail.com>
2022-11-29 14:56:24 -0500

    Createed yellow reaction branch

--- helperFunctions.py
+++ helperFunctions.py
@@ -6,6 +6,8 @@ hub = PrimeHub()
 motors = MotorPair('C', 'D')
 color = LightSensor('B')
 matrix = hub.light_matrix
+left_motor = Motor('C')
+right_motor = Motor('D')
 
 # If red is sensed by light sensor, the robot's
 # default speed will go down by 1.5 times.
@@ -22,6 +24,23 @@ def greenDetected():
     matrix.show_image('HAPPY')
     if base <= 60:
         motors.set_default_speed(base * 1.5)
+        
+# create '!' on matrix
+def showCaution():
+    hub.light_matrix.set_pixel(2, 0)
+    hub.light_matrix.set_pixel(2, 1)
+    hub.light_matrix.set_pixel(2, 2)
+    hub.light_matrix.set_pixel(2, 4)
+            
+# If yellow is sensed by light sensor, the robot's
+# default speed will go up by 1.5 times.
+def yellowDetected():
+    base = motors.get_default_speed()
+    showCaution()
+    if base <= 60:
+        left_motor.run_for_rotations(0.25,base * 1)
+        right_motor.run_for_rotations(0.25,base * -1)
+        motors.set_default_speed(base)
 
 # If violet is detected by the light sensor, a 
 # counter will be started and increased storing 
