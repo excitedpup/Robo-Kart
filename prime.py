ff2a141fe4c21a2121aea709940d0e93cbebf75b
Jake Findlow <findlo38@students.rowan.edu>
2022-12-08 19:07:32 -0500

    Update prime.py

--- prime.py
+++ prime.py
@@ -3,23 +3,28 @@
 from spike import PrimeHub,ColorSensor, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, Timer
 from math import *
+import random
 
 # Initialize
 timer = Timer()
 racetimer = Timer()
-color_sensor_reactions = ColorSensor('E')
+color_sensor_reactions = ColorSensor('A')
+colorLeft = ColorSensor('A')
+colorRight = ColorSensor('B')
 motor_pair = MotorPair('C','D')
 hub = PrimeHub()
 matrix = hub.light_matrix
 left_motor = Motor('C')
 right_motor = Motor('D')
-distance_sensor =DistanceSensor('F')
+#distance_sensor =DistanceSensor('F')
 
 
 counter = 0
 lap1 = 0
 lap2 = 0
 lap3 = 0
+colorStatus = False
+colorTime = 0
 
 
 def beep(amount):
@@ -90,9 +95,6 @@ def redDetected():
     base = motor_pair.get_default_speed()
     matrix.show_image('SAD')
     motor_pair.start(steering=0, speed=int(base/1.5))
-    wait(2)
-    motor_pair.start(steering=0, speed=base)
-    hub.light_matrix.off()
 
 # If green is sensed by light sensor, the robot's
 # default speed will go up by 1.5 times.
@@ -100,9 +102,6 @@ def greenDetected():
     base = motor_pair.get_default_speed()
     matrix.show_image('ARROW_N')
     motor_pair.start(steering=0, speed=int(base *1.5))
-    wait(2)
-    motor_pair.start(steering=0, speed=base)
-    hub.light_matrix.off()
 
 # create '!' on matrix
 def showCaution():
@@ -125,6 +124,54 @@ def yellowDetected(duration, direction, pause):
         right_motor.stop()
     motor_pair.start(steering=0, speed=base)
     hub.light_matrix.off()
+
+def blueDetected():
+    base = motor_pair.get_default_speed()
+    #Goes at 3/4 current speed if going fast and half speed if going slow
+    fast = False
+    waterSpeed = 0
+    if base >= 35:
+        waterSpeed = int(base * 3/4)
+        fast = True
+    else:
+        waterSpeed = int(base/2)
+
+    while colorLeft.get_color() != 'white' or colorRight.get_color() != 'white':
+        #Boundry detected and if going fast "hits" the boundary, stops, and adjusts to get back on track
+        if colorRight.get_color() == 'black':
+            if fast:
+                motor_pair.stop()
+                wait_for_seconds(2)
+                #turnLeft()
+                waterSpeed = int(base/2)
+                fast = False
+            else:
+                #turnLeft()
+                motor_pair.start(steering=0, speed=int(base/2))
+        elif colorLeft.get_color() == 'black':
+            if fast:
+                motor_pair.stop()
+                wait_for_seconds(2)
+                #turnRight()
+                waterSpeed = int(base/2)
+                fast = False
+            else:
+                #turnRight()
+                motor_pair.start(steering=0, speed=int(base/2))
+        else:
+            motor_pair.start(steering=0, speed=waterSpeed)
+
+    motor_pair.set_default_speed(base)
+
+def turnRight(base = 35):
+    steer = random.randint(20,30)
+    while isColor(black, color_sensor_reactions.get_rgb_intensity(), 40):
+        motor_pair.start(steering=steer, speed=base)
+
+def turnLeft(base = 35):
+    steer = -(random.randint(20,30))
+    while isColor(black, color_sensor_reactions.get_rgb_intensity(), 40):
+        motor_pair.start(steering=steer, speed=base)
 
 # If violet is detected by the light sensor, a
 # counter will be started and increased storing
@@ -207,7 +254,7 @@ def main(red, green, yellow, blue, violet, black, white, duration=15):
     global lap3
     fastestLap = 0
 
-    while (timer.now() < duration):     
+    while (timer.now() < duration):
         if isColor(red, color_sensor_reactions.get_rgb_intensity(), 80):
             print("red")
             redDetected()
@@ -254,5 +301,74 @@ def main(red, green, yellow, blue, violet, black, white, duration=15):
     print(fastestLap)
 
 
-red, green, yellow, blue, violet, black, white = calibrateSensor(color_sensor_reactions)
-main(red=red, green=green, yellow=yellow, blue=blue, violet=violet, black=black, white=white, duration=60)
+#red, green, yellow, blue, violet, black, white = calibrateSensor(color_sensor_reactions)
+#main(red=red, green=green, yellow=yellow, blue=blue, violet=violet, black=black, white=white, duration=60)
+
+
+def main2(red, green, yellow, blue, violet, black, white, duration=15):
+    timer.reset()
+    racetimer.reset()
+    print(racetimer.now())
+    beep(1)
+    base = motor_pair.set_default_speed(35)
+    motor_pair.start(steering=0, speed=base)
+
+    global counter
+    global lap1
+    global lap2
+    global lap3
+    global colorStatus
+    global colorTime
+    fastestLap = 0
+    base = 35
+
+    while (timer.now() < duration):
+
+        if not colorStatus:
+            motor_pair.start(steering=0, speed=base)
+            if isColor(red, color_sensor_reactions.get_rgb_intensity(), 80):
+                print("red")
+                colorTime = timer.now() + 2
+                redDetected()
+            elif isColor(green, color_sensor_reactions.get_rgb_intensity(), 80):
+                print("green")
+                colorTime = timer.now() + 2
+                greenDetected()
+            elif isYellow(yellow, color_sensor_reactions.get_rgb_intensity(), 80):
+                print("yellow")
+                yellowDetected(2, 100, .1)
+            elif isColor(blue, color_sensor_reactions.get_rgb_intensity(), 80):
+                print("blue")
+        else:
+            if colorTime < timer.now():
+                colorStatus = False
+                hub.light_matrix.off()
+
+
+        if isColor(violet, color_sensor_reactions.get_rgb_intensity(), 60):
+            print("violet")
+            #print(racetimer.now())
+            # fastestLap declaration here can be taken out I think!!!
+            fastestLap = raceTimer(racetimer.now())
+            counter = counter + 1
+            wait(1) # CHANGE BACK WHEN YOU STOP LIFTING BOT UP/BOT ABLE TO TRACK FOLLOW
+
+        if isColor(black, color_sensor_reactions.get_rgb_intensity(), 40):
+            print("black")
+
+        if distance_sensor.get_distance_cm() == None:
+            pass
+        elif distance_sensor.get_distance_cm() <= 30:
+            print(distance_sensor.get_distance_cm())
+            print("It got here")
+            hover(30)
+
+    motor_pair.stop()
+    if lap1<lap2 and lap1<lap2:
+        fastestLap = lap1
+    elif lap2<lap1 and lap2<lap3:
+        fastestLap = lap2
+    else:
+        fastestLap = lap3
+    print('Fastest Lap is:')
+    print(fastestLap)
